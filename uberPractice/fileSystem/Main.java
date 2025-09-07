e1f2f45c350a7332d34e321895790befe19e674c
Prakhar Sharma <prakharsharma@Prakhars-MacBook-Pro.local>
2025-09-07 21:32:53 +0530

    filesystem

--- uberPractice/fileSystem/Main.java
+++ uberPractice/fileSystem/Main.java
@@ -5,12 +5,12 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         FileSystem fs=new FileSystem();
-        new Thread(()->fs.mkdir("/abc/cd")).start();
-        new Thread(()->fs.cd("./abc")).start();        
-        new Thread(()->fs.cd("/")).start();
+        new Thread(()->fs.mkdir("/abc/cd/ef")).start();
+        // new Thread(()->fs.cd("./abc")).start();        
+        new Thread(()->fs.cd("/abc/./cd")).start();
         // try{
         //     Thread.sleep(2000);
         // }catch(Exception e){}
-        new Thread(()->System.out.println(fs.pwd())).start();        
+        new Thread(()->System.out.println(fs.pwd())).start();      
     }
 }
